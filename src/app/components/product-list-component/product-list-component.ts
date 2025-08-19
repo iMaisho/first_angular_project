4faa3feb341e03059ce3c61229549712bca541ad
iMaisho <antonin.mingam@gmail.com>
2025-08-19 15:03:22 +0200

    ajoute la release date au produit

--- src/app/components/product-list-component/product-list-component.ts
+++ src/app/components/product-list-component/product-list-component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { Product } from '../../interfaces/product';
+import { CurrencyPipe, DatePipe, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-list-component',
-  imports: [],
+  imports: [DatePipe, TitleCasePipe, CurrencyPipe],
   templateUrl: './product-list-component.html',
   styleUrl: './product-list-component.css'
 })
 export class ProductListComponent {
 
-  products: Array<Product> = [{id: 1, name: "Echelle", price:30, inStock: true}, {id: 2, name: "Clous", price:5, inStock: false}
+  products: Array<Product> = [{id: 1, name: "Echelle", price:30, inStock: true, releaseDate: new Date()}, {id: 2, name: "Clous", price:5, inStock: false, releaseDate: new Date()}
 
   ]
 
